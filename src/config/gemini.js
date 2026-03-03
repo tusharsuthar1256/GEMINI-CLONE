ba3e2360349c0797b3f0b11b99b336acb2a79cf5
tusharsuthar1256 <tusharsuthar081@gmail.com>
2026-03-03 10:09:51 +0530

    commited correct changes

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -19,7 +19,7 @@ const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.5-flash",
 });
 
 const generationConfig = {
